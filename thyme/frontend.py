5c76fc706f478de7f2803fab5ddb571c98ee3e75
Adam Avramov <fallenblood@gmail.com>
2013-07-08 20:21:05 +0300

    Implemented basic timeline.

--- thyme/frontend.py
+++ thyme/frontend.py
@@ -1,13 +1,15 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+from random import randrange
 
 import cherrypy
 from jinja2 import Environment, FileSystemLoader
+from sqlalchemy import desc
 
 from .database import init_db, Entry
 from .settings import TEMPLATE_DIR
 
 
-class ThymeFace:
+class ThymeFace(object):
     def index(self):
         today = date.today()
         today_start = datetime(today.year, today.month, today.day, 0, 0, 0)
@@ -15,10 +17,33 @@ class ThymeFace:
 
         entries = self.db.query(Entry). \
             filter(Entry.time >= today_start, Entry.time <= today_end). \
-            order_by(Entry.time)
+            order_by(desc(Entry.time))
+
+        timeline = []
+        total_time = timedelta(seconds=0)
+        end_time = datetime.now()
+
+        for entry in entries:
+            duration = end_time - entry.time
+            total_time += duration
+            end_time = entry.time
+            timeline.append({'title': entry.title,
+                             'duration': duration})
+
+        for entry in timeline:
+            entry.update({'width': entry['duration'] / total_time * 100,
+                          'color': 'rgb(%s,%s,%s)' % (randrange(255),
+                                                      randrange(255),
+                                                      randrange(255),)})
 
         template = self.env.get_template('frontend.html')
-        return template.render(entries=entries)
+
+        context = {
+            'timeline': timeline,
+            'total_time': total_time
+        }
+
+        return template.render(context)
 
     def __init__(self, db):
         self.db = db
